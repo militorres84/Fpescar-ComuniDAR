e04889dfa40cba699914c582570ad131005ac888
mili torres <militorres018@gmail.com>
2023-11-02 03:33:23 -0300

    first posts

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className='logo'>ComuniDAR</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+
+        </nav>
+      </header>
+      <div className='post'>
+        <div className="image">
+        <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
+        </div>
+        <div className='text'>
+        <h2>Te acompañamos a crecer como profesional y como persona.</h2>
+        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className="image">
+        <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
+        </div>
+        <div className='text'>
+        <h2>Te acompañamos a crecer como profesional y como persona.</h2>
+        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className="image">
+        <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
+        </div>
+        <div className='text'>
+        <h2>Te acompañamos a crecer como profesional y como persona.</h2>
+        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
