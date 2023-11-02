14b8f3c674d89b0eb2fed53494fa5ff59a26c293
mili torres <militorres018@gmail.com>
2023-11-02 04:10:02 -0300

    creating posts and routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,39 @@ function App() {
         <div className="image">
         <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
         </div>
-        <div className='text'>
+        <div className='texts'>
         <h2>Te acompañamos a crecer como profesional y como persona.</h2>
-        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        <p className="info">
+          <a className="author">Camila from ComuniDAR</a>
+          <time>2023-11-2 10:00</time>
+        </p>
+        <p className='summary'>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
         </div>
       </div>
       <div className='post'>
         <div className="image">
         <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
         </div>
-        <div className='text'>
+        <div className='texts'>
         <h2>Te acompañamos a crecer como profesional y como persona.</h2>
-        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        <p className="info">
+          <a className="author">Camila from ComuniDAR</a>
+          <time>2023-11-2 10:00</time>
+        </p>
+        <p className='summary'>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
         </div>
       </div>
       <div className='post'>
         <div className="image">
         <img src="https://imgs.search.brave.com/0UHDiuFgs0WueLTGqiz3U47yJ9g0ZCgT03F_fUg27Cs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/cmV0cmF0by1qb3Zl/bi1ob21icmUtbmVn/b2Npb3MtdHJhYmFq/YW5kb18yMy0yMTQ4/OTA4ODkzLmpwZz9z/aXplPTYyNiZleHQ9/anBn" alt="working" />
         </div>
-        <div className='text'>
+        <div className='texts'>
         <h2>Te acompañamos a crecer como profesional y como persona.</h2>
-        <p>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
+        <p className="info">
+          <a className="author">Camila from ComuniDAR</a>
+          <time>2023-11-2 10:00</time>
+        </p>
+        <p className='summary'>CommuniDAR es un espacio de acompañamiento que reune los mejores consejos y mentorías para que saques el mejor potencial de tus aptitudes y aprendas a cada paso sobre tus zonas de mejora.</p>
         </div>
       </div>
     </main>
